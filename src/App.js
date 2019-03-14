1b00def380c21334be67be8f2cbf9544b9fbccab
Victor Kane <victorkane@gmail.com>
2019-03-14 05:45:24 -0400

    SearchBar consuming Context using React Context Consumer component as part of rendering

--- src/App.js
+++ src/App.js
@@ -109,45 +109,30 @@ class ProductTable extends React.Component {
 }
 
 class SearchBar extends React.Component {
-/*
-    constructor(props) {
-          super(props);
-          this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
-          this.handleInStockChange = this.handleInStockChange.bind(this);
-        }
-    
-    handleFilterTextChange(e) {
-          this.props.onFilterTextChange(e.target.value);
-        }
-    
-    handleInStockChange(e) {
-          this.props.onInStockChange(e.target.checked);
-        }
-*/
-    
     render() {
           return ( 
-					<div></div>
-/*
+					      <AppContext.Consumer>
+								 {context =>
                   <form>
                     <input
                       type="text"
                       placeholder="Search..."
-                      value={this.props.filterText}
-                      onChange={this.handleFilterTextChange}
+                      value={context.filterText}
+                      onChange={(e) => context.handleFilterTextChange(e.target.value)}
                     />
                     <p>
                       <input
                         type="checkbox"
-                        checked={this.props.inStockOnly}
-                        onChange={this.handleInStockChange}
+                        checked={context.inStockOnly}
+                        onChange={(e) => context.handleInStockChange(e.target.checked)}
                       />
                       {' '}
                       Only show products in stock
                     </p>
                   </form>
-*/
-                );
+								 }
+					      </AppContext.Consumer>
+                )
         }
 }
 
